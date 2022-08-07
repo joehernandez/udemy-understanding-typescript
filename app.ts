48aedd7c8cf68728aef2c77f2d9c01a6db4c6fc1
Joe Hernandez <joe.hernandez@instructure.com>
2022-08-07 14:04:01 -0500

    Completed Section05 #61

--- app.ts
+++ app.ts
@@ -1,9 +1,21 @@
-function add(n1: number, n2: number) : number{
-    return n1 + n2
-}
+class Department {
+    name: string
+  
+    constructor(n: string) {
+      this.name = n
+    }
+  }
 
-const number1 = 5
-const number2 = 2.8
+const dept = new Department('Accounting')
+console.log('Department', dept)
 
-const result = add(number1, number2)
-console.log(result)
+// function add(n1: number, n2: number) : number{
+//     return n1 + n2
+// }
+
+// const number1 = 5
+// const number2 = 2.8
+
+// const result = add(number1, number2)
+// console.log(result)
+// console.log('made a change')
