647c838556c5b73665ba6668abc6333757487219
Joe Hernandez <joe.a.hernandez@live.com>
2022-08-07 21:10:36 -0500

    Finished Section05 #68 - Static Methods & Properties

--- app.ts
+++ app.ts
@@ -1,13 +1,69 @@
 class Department {
-    name: string
+    // private id: string
+    // private name: string
+    private employees: string[] = []
   
-    constructor(n: string) {
-      this.name = n
+    // static property
+    static fiscalYear = 2020
+
+    constructor(private id: string, public readonly name: string) {
+        // this.id = id
+        // this.name = name
+        // to access static property in instance-related logic, use the class name
+        // console.log(Department.fiscalYear)
+    }
+
+    // static methods
+    static createEmployee(empName: string) {
+        return { name: empName }
+    }
+
+    describe(this: Department) {
+        console.log(`Department: ${this.name}: ${this.id}` )
+    }
+
+    addEmployee(employee: string) {
+        this.employees.push(employee)
+    }
+
+    printEmployeeInformation() {
+        console.log(this.employees.length)
+        console.log(this.employees)
     }
   }
 
-const dept = new Department('Accounting')
-console.log('Department', dept)
+class ITDepartment extends Department {
+    constructor(id: string, private admins: string[] = []) {
+        super(id, 'IT') // Must call super(...) before using 'this' keyword
+    }
+
+    addAdmin(adminName: string) {
+        this.admins.push(adminName)
+    }
+
+    printAdmins() {
+        console.log(`Department: ${this.name}`)
+        console.log(this.admins)
+    }
+}
+
+// static methods & properties
+const emp1 = Department.createEmployee('Employee1')
+const fiscalYear = Department.fiscalYear
+console.log(`New employee: ${emp1.name} | Fiscal Year: ${fiscalYear}`)
+
+const it = new ITDepartment('2')
+it.addAdmin('JoeMama')
+it.addAdmin('PhilipTheNickel')
+it.printAdmins()
+
+const accounting = new Department('1','Accounting')
+// accounting.name = 'New Name' => Results in TypeScript error because field is readonly
+accounting.addEmployee('Joe')
+accounting.addEmployee('Manu')
+
+accounting.describe()
+accounting.printEmployeeInformation()
 
 // function add(n1: number, n2: number) : number{
 //     return n1 + n2
